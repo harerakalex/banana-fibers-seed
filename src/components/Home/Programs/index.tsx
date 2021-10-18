7f4b1b516b3c9d696dd1a87ba1539f726d07441e
harerakalex <hareraloston@gmail.com>
2021-10-18 21:13:25 +0200

    Add actual programms

--- src/components/Home/Programs/index.tsx
+++ src/components/Home/Programs/index.tsx
@@ -15,10 +15,13 @@ const Programs: FC = () => (
           <Card style={{ width: '100%' }}>
             <Card.Img variant="top" src={image1} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Campaign against teenage pregnancies root cause mindset</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the
-                card's content.
+                where we will be having different topics, schecks, debates, conferences,
+                competitions and much more; with the aim of discussing with youth subjects in line
+                with their mindsets and perceptions towards their lives circumstances, and ways the
+                mindset can be controlled to give them the right effective reactions or actions
+                towards those different life circumstances they face in their day to day lives.
               </Card.Text>
               <Button variant="primary">Donate</Button>
             </Card.Body>
@@ -29,10 +32,12 @@ const Programs: FC = () => (
           <Card style={{ width: '100%' }}>
             <Card.Img variant="top" src={image2} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Career guidance for teenagers</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the
-                card's content.
+                In this program we focus on the dreams those teenagers and youth in general have,
+                and meet them at their schools( in secondary schools) and both boys and girls alike,
+                because we believe pregnancy doesn't occur from the participation of the girl child
+                alone, even the boy child takes part in this act.
               </Card.Text>
               <Button variant="primary">Donate</Button>
             </Card.Body>
@@ -43,10 +48,11 @@ const Programs: FC = () => (
           <Card style={{ width: '100%' }}>
             <Card.Img variant="top" src={image3} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Financial support</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the
-                card's content.
+                In providing basic supports to poor teenage mothers, we visit them, get to
+                understand their situation, collect their testimonies for the next generation
+                lessons, and we give them basic needs for every day to day life
               </Card.Text>
               <Button variant="primary">Donate</Button>
             </Card.Body>
